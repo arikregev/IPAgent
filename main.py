a26528de29ad07300fb67c92ca70b7eefca24097
Arik Regev <arik@aregev.com>
2022-11-19 03:20:15 +0200

    update main basic flow

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from mainfunctions import queryMyIP
+from mainfunctions import queryMyIP, openCacheFile, readJsonFile, writeIPtoJsonFile, GLOBAL_EXIT_CODE
 
 def main():
     #Step 1: query my current IP address.
     current = queryMyIP()
-    #Step 2: Query current IP address from Cloud Provider FW.
 
-    #Step 3: if there is no match between 1 and 2, push 1 into 2.
+    #Step 2: check aginst local cache.
+    file = openCacheFile('IPAgent.json')
+    json = readJsonFile(file)
+    if(json == None):
+        #Step 3: if there is no match between 1 and 2, push 1/2 into cloud provider fw.
+        writeIPtoJsonFile(current)
+    else:
+        #close all and bye
+        print()
 
-    print(current)
+    file.close()
 
 if __name__ == "__main__":
     main()
